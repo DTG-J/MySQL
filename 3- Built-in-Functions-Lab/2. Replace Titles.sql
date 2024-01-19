411d663a29ac2f8bd6e7647cabaf125692c1a1e6
DTG-J <grozdanovdt@abv.bg>
2024-01-19 16:59:46 +0200

    Create 2. Replace Titles.sql

---
+++ 3- Built-in-Functions-Lab/2. Replace Titles.sql
@@ -0,0 +1,10 @@
+# 2.	Replace Titles
+/* Write a SQL query to find books which titles start with "The" and replace the substring with 3 asterisks.
+ Retrieve data about the updated titles. 
+Order the result by id. Submit your query statements as Prepare DB & run queries. */
+
+SELECT REPLACE (`title`, "The", '***')
+AS Title
+FROM books
+WHERE SUBSTRING(title, 1, 3) = "The"
+ORDER by id; 

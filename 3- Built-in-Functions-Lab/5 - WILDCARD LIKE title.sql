19c564938a86f99946a7e909b848049b430e3001
DTG-J <grozdanovdt@abv.bg>
2024-01-20 14:04:40 +0200

    Create 5 - WILDCARD LIKE title.sql

---
+++ 3- Built-in-Functions-Lab/5 - WILDCARD LIKE title.sql
@@ -0,0 +1,6 @@
+# 5.	Harry Potter Books
+/* Write a SQL query to retrieve titles of all the Harry Potter books. Order the information by id. 
+Submit your query statements as Prepare DB & run queries. */
+select title
+from books
+where title like 'Harry Potter%'; 

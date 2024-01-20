0fa1f0754b16869d23a5f739599fd92f662b6120
DTG-J <grozdanovdt@abv.bg>
2024-01-20 12:43:38 +0200

    Update TIMESTAMPDIFF.sql

--- 3- Built-in-Functions-Lab/TIMESTAMPDIFF.sql
+++ 3- Built-in-Functions-Lab/TIMESTAMPDIFF.sql
@@ -2,3 +2,7 @@ SELECT `employee_id`, `first_name`, `last_name`,
 TIMESTAMPDIFF(year, `hire_date`, '2017-05-31')
 AS 'Years In Service'
 FROM `employees`;
+
+SELECT concat(first_name, ' ', last_name) AS 'Full Name',
+TIMESTAMPDIFF(DAY, born, died) AS 'Days Lived'
+FROM authors;

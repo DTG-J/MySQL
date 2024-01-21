2c6db4c7c721c7522e98a94318fa3179160a1686
DTG-J <grozdanovdt@abv.bg>
2024-01-21 20:44:43 +0200

    Create 8- Make Shared PK.sql

---
+++ Data Definition and Datatypes  tasks/8- Make Shared PK.sql
@@ -0,0 +1,11 @@
+# 8.	Change Primary Key
+/* Using SQL queries modify table users from the previous task. 
+First remove current primary key then create new primary key that would be combination of fields id and username. 
+The initial primary key name on id is pk_users. 
+Submit your query in Judge as Run skeleton, run queries & check DB.*/
+ALTER TABLE users
+DROP PRIMARY KEY;
+
+ALTER TABLE users
+DROP PRIMARY KEY,
+ADD PRIMARY KEY (id, username);

6346e4037f458ff886a2dc90b775120f6603a49a
DTG-J <grozdanovdt@abv.bg>
2024-01-22 20:39:17 +0200

    Create 2-Last name contains.sql

---
+++ 3- Built-in-Functions-EXERCISE/2-Last name contains.sql
@@ -0,0 +1,12 @@
+# 2. Find Names of All Employees by Last Name
+
+/*Write a SQL query to find first and last names of all employees whose last name contains "ei" (case insensitively). Order the information by id. Submit your query statements as Prepare DB & run queries.
+Example
+first_name	last_name
+Kendall	Keil
+Christian	Kleinerman
+ */
+ 
+ SELECT first_name, last_name
+ FROM employees
+ WHERE last_name LIKE '%ei%';

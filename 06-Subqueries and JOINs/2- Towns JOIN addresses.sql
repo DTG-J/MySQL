fdaef44086630099ea5699d0cbc22b50ffdd20ee
DTG-J <grozdanovdt@abv.bg>
2024-01-27 13:59:13 +0200

    Update 2- Towns JOIN addresses.sql

--- 06-Subqueries and JOINs/2- Towns JOIN addresses.sql
+++ 06-Subqueries and JOINs/2- Towns JOIN addresses.sql
@@ -8,3 +8,11 @@ JOIN towns ON towns. town_id = addresses. town_id
 where towns.name = 'San Francisco' OR name = 'Sofia' OR name = 'Carnation'
 ORDER BY town_id, address_id; 
 
+SELECT  t.town_id,
+		t.name AS 'town_name',
+        a.address_text
+FROM addresses AS a
+JOIN towns AS t ON a.town_id = t.town_id
+WHERE t.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER by t.town_id, a. address_id; 
+

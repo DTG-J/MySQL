149e36110423e317cc4f613fa52e2b7a1c87385e
DTG-J <grozdanovdt@abv.bg>
2024-01-27 15:31:59 +0200

    Create 3-Employee details query.sql

---
+++ 2-Basic CRUD operations - EXERCISE/3-Employee details query.sql
@@ -0,0 +1,13 @@
+# Find salary of Each Employee 
+/* Write SQL query to find the first name, last name and salary of each employee. Sort the information by id. Submit your query statements as Prepare DB & run queries. 
+Example 
+first_name 	last_name 	salary 
+Guy 	Gilbert 	12500.00 
+Kevin 	Brown 	13500.00 
+Roberto 	Tamburello 	43300.00 
+*/
+SELECT first_name AS 'first name',
+		last_name AS 'last name',
+        salary
+FROM employees
+ORDER BY employee_id;

abd43982f7dbfa6d50a86f8e396383229554752a
DTG-J <grozdanovdt@abv.bg>
2024-01-27 15:27:44 +0200

    Create 2- Find all Department Names.sql

---
+++ 2-Basic CRUD operations - EXERCISE/2- Find all Department Names.sql
@@ -0,0 +1,11 @@
+# Find all Department Names
+/* 
+Write SQL query to find all department names. Sort the information by id. Submit your query statements as Prepare DB & run queries. 
+Example 
+name 
+Engineering 
+Tool Design 
+Sales */
+SELECT name
+FROM departments
+ORDER BY department_id;

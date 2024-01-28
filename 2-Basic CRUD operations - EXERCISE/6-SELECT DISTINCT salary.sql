0d55758def73460d7240c95e8b522ad8fd91df1b
DTG-J <grozdanovdt@abv.bg>
2024-01-28 18:28:21 +0200

    Create 6-SELECT DISTINCT salary.sql

---
+++ 2-Basic CRUD operations - EXERCISE/6-SELECT DISTINCT salary.sql
@@ -0,0 +1,10 @@
+# 06. Find All Different Employee’s Salaries 
+/* Write a SQL query to find all different employee's salaries. Show only the salaries. 
+Submit your query statements as Prepare DB & run queries.  
+Example 
+Salary 
+12500.00 
+13500.00 
+*/
+SELECT DISTINCT salary
+FROM employees;

83d43ac0e60ef319e3f57e5ab8d351887732ed4d
DTG-J <grozdanovdt@abv.bg>
2024-01-30 06:21:36 +0200

    Create 14-Sort Employees Table.sql

---
+++ 2-Basic CRUD operations - EXERCISE/14-Sort Employees Table.sql
@@ -0,0 +1,18 @@
+# 14-Sort Employees Table 
+/*Write a SQL query to sort all records in the еmployees table by the following criteria:  
+•	First by salary in decreasing order 
+•	Then by first name alphabetically 
+•	Then by last name descending 
+•	Then by middle name alphabetically 
+Sort the information by id. Submit your query statements as Prepare DB & run queries. 
+ */
+
+SELECT 
+  *
+FROM employees
+ORDER BY 
+    salary DESC,
+    first_name,
+    last_name DESC,
+    middle_name,
+    employee_id;

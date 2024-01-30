930d3525ed0b6a21f4b48496565d3eb1c95283b8
DTG-J <grozdanovdt@abv.bg>
2024-01-30 06:21:20 +0200

    Create 13-Find All Employees Except Marketing.sql

---
+++ 2-Basic CRUD operations - EXERCISE/13-Find All Employees Except Marketing.sql
@@ -0,0 +1,16 @@
+# 13-Find All Employees Except Marketing 
+/*Write a SQL query to find the first and last names of all employees whose department ID is different from 4. 
+Submit your query statements as Prepare DB & run queries. 
+Example 
+first_name 	last_name 
+Guy 	Gilbert 
+Roberto 	Tamburello 
+Rob 	Walters 
+ */
+ SELECT first_name, last_name
+ FROM employees
+ WHERE department_id != 4; 	
+ 
+  SELECT first_name, last_name
+ FROM employees
+ WHERE department_id NOT IN (4); 	

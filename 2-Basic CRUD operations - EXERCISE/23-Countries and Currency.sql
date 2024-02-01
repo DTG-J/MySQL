7e746eacfc45ff9a8a9ce84a38aed8616c045962
DTG-J <grozdanovdt@abv.bg>
2024-02-01 17:33:17 +0200

    Create 23-Countries and Currency.sql

---
+++ 2-Basic CRUD operations - EXERCISE/23-Countries and Currency.sql
@@ -0,0 +1,14 @@
+# 23-Countries and Currency (Euro / Not Euro) 
+/* Find all countries along with information about their currency. Display the country name, country code and information about its currency: either "Euro" or "Not Euro". Sort the results by country name alphabetically. Submit your query statements as Prepare DB & run queries. 
+Example 
+country_name 	country_code 	currency 
+Afghanistan 	AF 	Not Euro 
+Åland 	AX 	Euro 
+Albania 	AL 	Not Euro 
+*/
+SELECT 
+    country_name, 
+    country_code, 
+    CASE WHEN currency_code = 'EUR' THEN 'Euro' ELSE 'Not Euro' END AS currency
+FROM countries
+ORDER BY country_name;

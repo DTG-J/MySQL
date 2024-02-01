1aefdb02806bd5ba286399d5037eee8d9f498916
DTG-J <grozdanovdt@abv.bg>
2024-02-01 17:33:34 +0200

    Create 24- All Diablo Characters.sql

---
+++ 2-Basic CRUD operations - EXERCISE/24- All Diablo Characters.sql
@@ -0,0 +1,11 @@
+# 24. All Diablo Characters 
+/* Display the name of all characters in alphabetical order. Submit your query statements as Prepare DB & run queries. 
+Example 
+name 
+Amazon 
+Assassin 
+Barbarian 
+*/
+SELECT name
+FROM characters
+ORDER BY name;

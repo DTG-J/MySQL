1478f2dc21e2c55d72cd90669f52b0d32be0081f
DTG-J <grozdanovdt@abv.bg>
2024-02-01 17:35:57 +0200

    Create 1- Records' Count.sql

---
+++ 4-Data Aggregation -EXERCISE/1- Records' Count.sql
@@ -0,0 +1,8 @@
+# 1- Records' Count
+/*Import the database and send the total count of records to Mr. Bodrog. Make sure nothing got lost.
+Example:
+count
+162
+ */
+ SELECT COUNT(id)
+ FROM wizzard_deposits;

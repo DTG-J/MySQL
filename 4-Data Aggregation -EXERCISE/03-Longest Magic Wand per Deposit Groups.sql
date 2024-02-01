6e5b51ba025d88bc75dba9423047158a471cd9d0
DTG-J <grozdanovdt@abv.bg>
2024-02-01 17:36:32 +0200

    Create 03-Longest Magic Wand per Deposit Groups.sql

---
+++ 4-Data Aggregation -EXERCISE/03-Longest Magic Wand per Deposit Groups.sql
@@ -0,0 +1,13 @@
+# 03-Longest Magic Wand per Deposit Groups
+/* For wizards in each deposit group show the longest magic wand. 
+Sort result by longest magic wand for each deposit group in increasing order, then by deposit_group alphabetically. Rename the new column appropriately.
+Example:
+deposit_group	longest_magic_wand
+Human Pride	30
+*/
+SELECT deposit_group, MAX(magic_wand_size) AS 'longest_magic_wand'
+FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY longest_magic_wand, deposit_group;
+
+SELECT 

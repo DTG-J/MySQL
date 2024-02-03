a4b2d6b227cab71fec3813a8193de9a1795420e6
DTG-J <grozdanovdt@abv.bg>
2024-02-03 11:26:49 +0200

    Create 18-Departments Total Salaries.sql

---
+++ 4-Data Aggregation -EXERCISE/18-Departments Total Salaries.sql
@@ -0,0 +1,12 @@
+# 18-Departments Total Salaries 
+/* Create a query which shows the total sum of salaries for each department. Order by department_id.
+Your query should return:	
+•	department_id
+Example:
+department_id	total_salary
+1	241000.00
+*/
+SELECT department_id, sum(salary) AS 'total_salary'
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;

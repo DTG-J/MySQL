b7a79ba4e13e2def3e667e8008de87c179f2048b
DTG-J <grozdanovdt@abv.bg>
2024-02-04 10:45:25 +0200

    Create 2-Employees with Salary Above Number.sql

---
+++ 07-Functions and Procedures - EXERCISE/2-Employees with Salary Above Number.sql
@@ -0,0 +1,20 @@
+# 2-Employees with Salary Above Number
+/* Create stored procedure usp_get_employees_salary_above that accept a decimal number (with precision, 4 digits after the decimal point) as parameter and return all employee's first and last names whose salary is above or equal to the given number. The result should be sorted by first_name then by last_name alphabetically and id ascending. Submit your query statement as Run skeleton, run queries & check DB in Judge.
+Example
+Supplied number for that example is 45000.
+first_name	last_name
+Amy	Alberts
+Brian	Welcker
+Dylan	Miller
+*/
+DELIMITER $usp_get_employees_salary_above
+CREATE PROCEDURE usp_get_employees_salary_above (number DECIMAL (10,4))
+BEGIN
+	SELECT first_name, last_name
+	FROM employees
+	WHERE salary >= number 
+	ORDER BY first_name, last_name, employee_id;
+END$
+
+DELIMITER ; 
+CALL usp_get_employees_salary_above (45000);

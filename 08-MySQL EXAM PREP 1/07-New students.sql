bdce35a3041a668a0b0554011ebb335dfa61a8ad
DTG-J <grozdanovdt@abv.bg>
2024-02-06 18:28:52 +0200

    Create 07-New students.sql

---
+++ 08-MySQL EXAM PREP 1/07-New students.sql
@@ -0,0 +1,9 @@
+# 07-New students
+SELECT CONCAT (s.first_name, ' ', s.last_name) AS full_name,
+SUBSTRING(email, 2, 10) AS username,  
+REVERSE(s.phone) AS password
+FROM students s
+LEFT JOIN students_courses st 
+ON s.id = st.student_id
+WHERE st.course_id IS NULL
+ORDER BY password DESC; 

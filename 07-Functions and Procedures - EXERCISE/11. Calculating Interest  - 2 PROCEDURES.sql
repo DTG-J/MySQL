c4ed8ffe3b50339beaae70bff82461cebf6f71da
DTG-J <grozdanovdt@abv.bg>
2024-02-10 18:26:19 +0200

    Create 11. Calculating Interest  - 2 PROCEDURES.sql

---
+++ 07-Functions and Procedures - EXERCISE/11. Calculating Interest  - 2 PROCEDURES.sql
@@ -0,0 +1,38 @@
+# 11. Calculating Interest
+DELIMITER &&
+CREATE PROCEDURE usp_calculate_future_value_for_account(id INT, interest_rate DECIMAL(19,4))
+BEGIN
+    SELECT a.`id` AS 'account_id', ah.`first_name`, ah.`last_name`, 
+    a.`balance` AS 'current_balance', ufn_calculate_future_value(a.balance, interest_rate, 5) AS 'balance_in_5_years'
+    FROM `account_holders` AS ah
+    JOIN `accounts` AS a ON a.`account_holder_id` = ah.`id`
+    WHERE a.`id` = id;
+END &&
+DELIMITER ;
+ 
+ 
+ # 10. Future Value Function
+DELIMITER &&
+CREATE FUNCTION ufn_calculate_future_value(sum DECIMAL(19, 4), yearly_interest DOUBLE, years INT)
+RETURNS DECIMAL(19, 4)
+DETERMINISTIC
+BEGIN
+    DECLARE future_sum DECIMAL(19, 4);
+    SET future_sum := sum * POW(1 + yearly_interest, years);
+    RETURN future_sum;
+END &&
+DELIMITER ;
+ 
+ ======================================================
+# 11. Calculating Interest
+DELIMITER &&
+CREATE PROCEDURE usp_calculate_future_value_for_account(id INT, interest_rate DECIMAL(19,4))
+BEGIN
+    SELECT a.`id` AS 'account_id', ah.`first_name`, ah.`last_name`, 
+    a.`balance` AS 'current_balance', ufn_calculate_future_value(a.balance, interest_rate, 5) AS 'balance_in_5_years'
+    FROM `account_holders` AS ah
+    JOIN `accounts` AS a ON a.`account_holder_id` = ah.`id`
+    WHERE a.`id` = id;
+END &&
+DELIMITER ;
+ 

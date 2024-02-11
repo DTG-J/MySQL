31aa16bbef1639b324b80b31de43c8730799d25e
DTG-J <grozdanovdt@abv.bg>
2024-02-11 09:03:53 +0200

    Create 6. Employees Hired After.sql

---
+++ 06-Subqueries and JOINs - EXERCISE/6. Employees Hired After.sql
@@ -0,0 +1,7 @@
+# 6. Employees Hired After
+SELECT e.first_name, e.last_name, e.hire_date, d.name
+FROM employees AS e
+	INNER JOIN departments AS d
+	ON e.department_id = d.department_id
+WHERE d.name IN ('Sales', 'Finance') AND e.hire_date > '1999-01-01 00:00:00'
+ORDER BY e.hire_date;

6d4508ca0cee1ecf557495d2007bada64668c99f
DTG-J <grozdanovdt@abv.bg>
2024-02-12 18:48:47 +0200

    Create 13. Count Mountain Ranges.sql

---
+++ 06-Subqueries and JOINs - EXERCISE/13. Count Mountain Ranges.sql
@@ -0,0 +1,7 @@
+# 13. Count Mountain Ranges
+SELECT c.`country_code`, COUNT(m.`mountain_range`) AS `mountain_range` FROM `mountains` AS m 
+     INNER JOIN `mountains_countries` AS c 
+     ON m.`id` = c.`mountain_id`
+WHERE c.`country_code` IN ('BG', 'RU', 'US')
+GROUP BY c.`country_code`
+ORDER BY `mountain_range` DESC;

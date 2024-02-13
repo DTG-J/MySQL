d8554ebae07694a7fd548d3647950eed70e1b76c
DTG-J <grozdanovdt@abv.bg>
2024-02-13 17:15:08 +0200

    Create 09. Preserve categories EXAM.sql

---
+++ MY SQL EXAM/09. Preserve categories EXAM.sql
@@ -0,0 +1,10 @@
+# 09.	Preserve categories
+SELECT p.id, p.name, 
+CASE 	WHEN area <= 100 THEN 'very small'
+				WHEN area < 1000 THEN 'small'
+				WHEN area < 10000 THEN 'medium'
+				WHEN area < 50000 THEN 'large'
+				ELSE 'very large' 
+		END AS 'category'
+FROM preserves p
+ORDER BY area DESC; 
